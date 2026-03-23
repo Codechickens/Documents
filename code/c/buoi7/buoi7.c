39272da7b19cc776b60ef066cc86a18d75c4bb36
linux-cachyos <mateirudante@gmail.com>
2026-03-23 16:25:06 +0700

    Cập nhật ví dụ 6.3 cho file buoi7.c

--- code/c/buoi7/buoi7.c
+++ code/c/buoi7/buoi7.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-	printf("%d\n",1);
-	return 0;
+	// VD6.3
+	int a=10;
+	//scanf("%d",&a);
+	if (a<10){
+		printf("so vua nhap nho hon 10\n");
+	} else if (a==10){
+		printf("so vua nhap bang 10\n");
+	} else if (a>10 && a<20){
+		printf("so vua nhap lon hon 10 va nho hon 20\n");
+	} else if (a==20){
+		printf("so vua nhap bang 20\n");
+	} else if (a>20){
+		printf("so vua nhap lon hon 20\n");
+	}
 }
