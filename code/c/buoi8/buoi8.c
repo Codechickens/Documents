16ac4892716ac4c003c76e0c0c43a88de7613c29
Bach Do Quang <mateirudante@gmail.com>
2026-03-31 02:00:25 +0700

    thêm một số bài tập của buổi 8

--- code/c/buoi8/buoi8.c
+++ code/c/buoi8/buoi8.c
@@ -25,6 +25,7 @@ int main(){
     printf("\nSo luong so thoa man dieu kien: %d", count);
 */
 
+/*    // BT 7.2 Tính lãi suất theo công thức lãi kép
     double von_goc, lai_suat, tong_tien, tien_lai;
     int thang;
     char ten[50];
@@ -57,5 +58,118 @@ int main(){
     printf("Thoi gian gui: %d thang\n", thang);
     printf("Tien lai nhan duoc: %.2f VND\n", tien_lai);
     printf("Tong so tien cuoi ky: %.2f VND\n\n", tong_tien);
+*/
+
+/*    // BT 7.3 Vẽ hình tam giác bằng dấu *
+    int n;
+    printf("Nhap chieu cao cua tam giac: ");
+    scanf("%d", &n);
+    for (int i=1;i<=n;i++){
+        for (int j=1; j<=i; j++){
+        printf("%c ", '*');
+        }
+        printf("\n");
+    }
+*/
+
+/*
+    // BT 7.4 Vẽ hình tam giác ngược bằng dấu *
+    int n;
+    printf("Nhap chieu cao cua tam giac nguoc: ");
+    scanf("%d", &n);
+    for (int i=n; i>=1; i--){
+        for (int j=1; j<=i; j++){
+        printf("%c ", '*');
+        }
+        printf("\n");
+    }
+*/
+
+/*
+    // BT 7.4b Vẽ hình tam giác cân bằng dấu *
+    for (int i = 1; i <= n; i++) {
+    // 1. In khoảng trắng để căn lề
+    for (int j = 1; j <= n - i; j++) {
+        printf(" ");
+    }
+
+    // 2. In dấu sao (số lượng sao = 2*i - 1)
+    for (int k = 1; k <= (2 * i - 1); k++) {
+        printf("*");
+    }
+
+    // 3. Xuống dòng
+    printf("\n");
+}
+*/
+
+/*
+    // BT 5 Liệt kê bảng chữ cái in hoa
+    for (char c = 'A'; c <= 'Z'; c++){
+        printf("%c ", c);
+    }
+*/
+
+/*    
+    // BT 6: Tính giai thừa của một số nguyên dương
+    int n;
+    int giai_thua = 1;
+    printf("Nhap so can tinh giai thua: ");
+    scanf("%d", &n);
+    if (n < 0){
+        printf("Giai thua chi tinh cho so nguyen duong!");
+        return 1;
+    }else if (n == 0){
+        printf("0! = 1\n");
+        return 0;
+    } else { 
+    for (int i=1; i<=n; i++){
+        giai_thua *= i;
+    }
+    printf("%d! = %d\n", n, giai_thua);
+    }
+*/
+
+/*
+    // BT 7 Tính tổng các số từ 1 đến n
+    int n;
+    int sum=0;
+    printf("Nhap so can tinh: ");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++){
+        sum += i;
+    }
+    printf("Tong cac so tu 1 den %d la: %d\n", n, sum);
+*/
+
+    int n;
+    int x;
+    int sum=0;
+    printf("Nhap so can tinh: ");
+    scanf("%d", &n);
+
+    printf("Nhap lua chon (1: tinh tong so le, 2: tinh tong so chan): ");
+    scanf("%d", &x);
+    switch (x){
+    case 2:
+        for (int i=1; i<=n; i++){
+            if (i%2 == 0){
+                sum += i;
+            }
+        }
+        printf("Tong cac so chan tu 1 den %d la: %d\n", n, sum);
+        break;
+    case 1:
+        for (int i=1; i<=n; i++){
+            if (i%2 != 0){
+                sum += i;
+            }
+        }
+        printf("Tong cac so le tu 1 den %d la: %d\n", n, sum);
+        break;
+    default:
+        printf("Lua chon khong hop le!\n");
+        break;
+    }
     return 0;
 }
