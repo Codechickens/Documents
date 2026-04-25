e908ee3314dd17f03a94e2f4afd131ea6d551a7b
Bach Do Quang <mateirudante@gmail.com>
2026-04-25 10:10:39 +0700

    thay đổi file buoi12.c

--- code/c/buoi12/buoi12.c
+++ code/c/buoi12/buoi12.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int Mangtangdan(int arr[], int n){
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return 0;
+}
+
+int Manggiamdan(int arr[], int n){
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] < arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return 0;
+}
+
+int Mangdacbiet(int a, int b, int c){
+    int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    return max;
+}
 
 int main(){
     // 10.1
@@ -21,11 +54,13 @@ int main(){
     */
 
     // 10.2
-    
+    /*
     int arr[10];
-    int sum1=0;
-    int sum2=0;
-    int sum3=0;
+    int sum1=0, sum2=0;
+    int count2=-1, count3=0, count4;
+    int x;
+    scanf("Nhap x: %d", &x);
+    getchar();
     for (int i = 0; i < 10; i++) {
         printf("Nhap phan tu thu %d: ", i + 1);
         scanf("%d", &arr[i]);
@@ -34,16 +69,47 @@ int main(){
             sum1+=arr[i];
         } else if (arr[i]<0){
             sum2+=arr[i];
+            count2++;
         } else {
-            sum3++;
+            count3++;
+        }
+
+        if (arr[i] == x){
+            count4=i+1;
+            break;
         }
     }
+    printf("\n---------------------------------\n");
     printf("Tong cac so duong: %d\n", sum1);
-    printf("Tong cac so am: %d\n", sum2);
-    printf("So cac so zero: %d\n", sum3);
-    
+    printf("Tong cac so am: %d\n", sum2/count2);
+    printf("So cac so zero: %d\n", count3);
+    printf("Vi tri x trong mang: %d\n", count4);
+    */
 
+    /*
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    Mangtangdan(arr, 10);
+    Manggiamdan(arr, 10);
+    printf("Mang sau khi sap xep tang dan: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    */
+
+    int n;
+    int arr[10];
+    //scanf("Nhap n: %d", &n);
+    getchar();
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
 
     return 0;
-
 }
+
