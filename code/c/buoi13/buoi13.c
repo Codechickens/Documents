6faef3dea0212f92fdb0f158e9e693cada7997ca
Dell_Precision_7530 <mateirudante@gmail.com>
2026-05-04 18:15:08 +0700

    Update buoi13.c

--- code/c/buoi13/buoi13.c
+++ code/c/buoi13/buoi13.c
@@ -21,6 +21,9 @@ int countEven(int *Num, int size) {
 
 int Maximum(int *a, int size){
     int max = *a;
+    if (size == 0 || *a == NULL) {
+        return NULL;
+    }
     for (int i = 1; i < size; i++) {
         if (*(a + i) > max) {
             max = *(a + i);
